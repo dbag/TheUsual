88e6c3cde31ee3d2b9b55c314a298832719e50ba
Jack Sprat <dbasil@gmail.com>
2011-11-08 10:01:16 -0600

    JSON play

--- lib/main.rb
+++ lib/main.rb
@@ -1,31 +1,9 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-require 'open-uri'
-require 'rexml/document'
+require 'rubygems'
+require 'json'
 
-# Fetches a del.icio.us user's recent bookmarks, and prints each one.
-def print_my_recent_bookmarks(username, password)
-  # Make the HTTPS request.
-  response = open('https://api.del.icio.us/v1/posts/recent', :http_basic_authentication => [username, password])
+puts [3, "three"].to_json
+puts JSON.parse('[4, "four"]') 
 
-  # Read the response entity-body as an XML document.
-  xml = response.read
-
-  # Turn the document into a data structure.
-  document = REXML::Document.new(xml)
-
-  # For each bookmark...
-  REXML::XPath.each(document, "/posts/post") do |e|
-    # Print the bookmark's description and URI
-    puts "#{e.attributes['description']}: #{e.attributes['href']}"
-  end
-end
-
-# Main program
-username, password = ARGV
-unless username and password
-  puts "Usage: #{$0} [username] [password]"
-  exit
-end
-print_my_recent_bookmarks(username, password)
